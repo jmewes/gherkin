b9ae41285b76dd533866580f4e18f6d66eb04fc1
Jan Mewes <jan.mewes@experimental-software.com>
2026-07-14 06:57:22 +0200

    Add implementation

--- utils/string_utils.go
+++ utils/string_utils.go
@@ -1,8 +1,33 @@
+// Copyright (C) 2026 Jan Mewes
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package utils
 
+import (
+	"strings"
+	"unicode"
+)
+
 func ToKebabCase(s string) string {
-	// Convert s to lower-case
-	// Replace all characters that do not match [A-Za-z0-9] with a dash
-	// Remove leading and trailing dashes
-	return s
+	runes := make([]rune, 0, len(s))
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			runes = append(runes, unicode.ToLower(r))
+		} else {
+			runes = append(runes, '-')
+		}
+	}
+	return strings.Trim(string(runes), "-")
 }
